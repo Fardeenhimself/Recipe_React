5b91068420f934c22ac21bd15a82f34a2669cbf5
Fardeenhimself <fardeenviper77@gmail.com>
2024-12-05 13:33:25 +0600

    UI edited, added skeleton

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,33 @@
 import { Search } from "lucide-react";
-import RecipeCard from "../components/RecipeCard";
+// import RecipeCard from "../components/RecipeCard";
+import { useEffect, useState } from "react";
+
+const APP_ID = "716a7a0f";
+const APP_KEY = "ab1a235df81e781b53e96e8989707365";
 
 const Home = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchRecipes = async (searchQuery) => {
+    setLoading(true);
+    setRecipes([]);
+
+    // try {
+    //   const res = await fetch(
+    //     `https://api.edamam.com/api/recipes/v2?app_id${APP_ID}&app_key${APP_KEY}&q=${searchQuery}&type=public`
+    //   );
+    //   const data = await res.json();
+    //   console.log(data);
+    // } catch (error) {
+    //   console.log(error.message);
+    // } finally {
+    //   // setLoading(false);
+    // }
+  };
+  useEffect(() => {
+    fetchRecipes("chicken");
+  }, []);
   return (
     <div className="bg-[#faf9fb] p-10 flex-1">
       <div className="max-w-screen-lg mx-auto">
@@ -23,7 +49,21 @@ const Home = () => {
           Popular Choices
         </p>
 
-        <RecipeCard />
+        <div className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {loading &&
+            [...Array(9)].map((_, index) => {
+              return (
+                <div key={index} className="flex flex-col gap-4 w-full">
+                  <div className="skeleton h-32 w-full"></div>
+                  <div className="flex justify-between">
+                    <div className="skeleton h-4 w-28"></div>
+                    <div className="skeleton h-4 w-28"></div>
+                  </div>
+                  <div className="skeleton h-4 w-1/2"></div>
+                </div>
+              );
+            })}
+        </div>
       </div>
     </div>
   );
